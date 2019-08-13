afd326e99a87da0990478a84b3b425d263ebe39f
Anita Kemp and Neha Malhotra <anita@>
2019-08-13 11:42:06 -0700

    Add website to make flashcards for vocabulary

---
+++ js/scripts.js
@@ -0,0 +1,131 @@
+$(document).ready(function(){
+  $(".click1").click(function() {
+    $("#p1").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click2").click(function() {
+    $("#p2").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click3").click(function() {
+    $("#p3").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click4").click(function() {
+    $("#p4").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click5").click(function() {
+    $("#p5").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click6").click(function() {
+    $("#p6").fadeToggle();
+
+
+  });
+});
+
+
+$(document).ready(function(){
+  $(".click7").click(function() {
+    $("#p7").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click8").click(function() {
+    $("#p8").fadeToggle();
+
+
+  });
+});
+
+
+$(document).ready(function(){
+  $(".click9").click(function() {
+    $("#p9").fadeToggle();
+
+
+  });
+});
+
+
+$(document).ready(function(){
+  $(".click10").click(function() {
+    $("#p10").fadeToggle();
+
+
+  });
+});
+
+
+$(document).ready(function(){
+  $(".click11").click(function() {
+    $("#p11").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click12").click(function() {
+    $("#p12").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click13").click(function() {
+    $("#p13").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click14").click(function() {
+    $("#p14").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click15").click(function() {
+    $("#p15").fadeToggle();
+
+
+  });
+});
+
+$(document).ready(function(){
+  $(".click16").click(function() {
+    $("#p16").fadeToggle();
+
+
+  });
+});
